67b0a910073afc0606036de2e7f5c0ed900fbca2
Richard <richard@communiteq.com>
2026-06-30 16:42:51 +0000

    Fix for anonymous homepage

--- javascripts/discourse/api-initializers/init-language-homepage.js
+++ javascripts/discourse/api-initializers/init-language-homepage.js
@@ -8,9 +8,10 @@ export default apiInitializer("1.8.0", (api) => {
 
   const entries = currentUser ? settings.logged_in_homepages : settings.anonymous_homepages;
   const match = entries.find((e) => (e.language_code || "").toLowerCase() === lang) || entries[0];
+  var homepage = null;
 
   if (match && match.homepage) {
-    let homepage = match.homepage;
+    homepage = match.homepage;
 
     if (homepage.startsWith("/")) {
       homepage = homepage.slice(1);
@@ -20,9 +21,33 @@ export default apiInitializer("1.8.0", (api) => {
     if (homepage.includes("?") && !homepage.endsWith("&")) {
       homepage = homepage + "&";
     }
+    console.log("Setting default homepage to: " + homepage);
     setDefaultHomepage(homepage);
+
     // https://meta.discourse.org/t/created-topic-sort/321521/15
     PreloadStore.remove("topic_list");
   }
-});
 
+  api.onPageChange((url) => {
+    // for anonymous users, we need to check if we are on a homepage
+    // that is NOT the correct homepage for the current language,
+    // and if so, redirect to the correct homepage
+    if (!currentUser) {
+      var router = api.container.lookup("service:router");
+      const currentTag = router.currentRoute?.params?.tag_name;
+      if (match && match.language_code && currentTag !== lang) {
+        // check if we are on the old home page
+        const oldEntry = entries.find((e) => (e.language_code || "").toLowerCase() === currentTag);
+        const oldHome = oldEntry?.homepage.split("?")[0] || null;
+        var urlWithoutQuery = url.split("?")[0];
+        if (urlWithoutQuery.startsWith("/")) {
+          urlWithoutQuery = urlWithoutQuery.slice(1);
+        }
+        if (oldHome == urlWithoutQuery) {
+          console.log("Redirecting to correct homepage for language: " + lang);
+          window.location.href = "/" + homepage;
+        }
+      }
+    }
+  });
+});
